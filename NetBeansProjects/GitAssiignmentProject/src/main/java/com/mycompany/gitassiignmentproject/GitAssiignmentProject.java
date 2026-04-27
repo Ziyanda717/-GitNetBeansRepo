7fe447af12fc8d48ec0f49aa0e51cc740498709d
Ziyanda Mazibuko <Ziyandaziphezinhle@gmail.com>
2026-04-27 20:41:39 +0200

    Initial commit: Added main Java file

---
+++ NetBeansProjects/GitAssiignmentProject/src/main/java/com/mycompany/gitassiignmentproject/GitAssiignmentProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitassiignmentproject;
+
+/**
+ *
+ * @author RT044
+ */
+
+public class GitAssiignmentProject{
+    public static void main(String[] args) {
+        System.out.println("Hello NetBeans. Welcome to version control using Git");
+    }
+}
