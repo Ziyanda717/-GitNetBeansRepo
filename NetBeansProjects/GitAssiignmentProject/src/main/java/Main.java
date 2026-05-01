48ae2fe4c423aab3154e4c2efca674f98bf88d57
Ziyanda Mazibuko <Ziyandaziphezinhle@gmail.com>
2026-05-01 18:54:44 +0200

    Added Hello from conflict-demo branch!

--- NetBeansProjects/GitAssiignmentProject/src/main/java/Main.java
+++ NetBeansProjects/GitAssiignmentProject/src/main/java/Main.java
@@ -7,8 +7,8 @@
  *
  * @author RT044
  */
-public class GitAssignmentProject {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello NetBeans. Welcome to version control using Git");
+       System.out.println("Hello from conflict-demo branch!");
     }
 }
